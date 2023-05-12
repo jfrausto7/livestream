4836c1d2cf4958a056c0df4011a01336ab0388ae
jfrausto7 <53204698+jfrausto7@users.noreply.github.com>
2023-05-11 23:23:34 -0400

    init config

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { PurrfectProvider } from "./PurrfectProvider";
+import { themeOptions } from "./Styles/themeOptions";
+import { ThemeProvider } from "@mui/material/styles";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log("HELLO");
+
+root.render(
+  <ThemeProvider theme={themeOptions}>
+    <PurrfectProvider>
+      <HashRouter basename="/">
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </HashRouter>
+    </PurrfectProvider>
+  </ThemeProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
